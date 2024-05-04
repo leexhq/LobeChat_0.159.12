gtla2025/hellogpt111

Add support for change default translation agent

src/app/settings/common/SystemAgent.tsx
@@ -1,0 +1,97 @@
+import { Form, type ItemGroup } from '@lobehub/ui';
+import { Form as AntForm, Select, SelectProps } from 'antd';
+import { createStyles } from 'antd-style';
+import isEqual from 'fast-deep-equal';
+import { MessageSquareMore } from 'lucide-react';
+import { memo, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { useSyncSettings } from '@/app/settings/hooks/useSyncSettings';
+import { ModelItemRender, ProviderItemRender } from '@/components/ModelSelect';
+import { FORM_STYLE } from '@/const/layoutTokens';
+import { useGlobalStore } from '@/store/global';
+import { modelProviderSelectors, settingsSelectors } from '@/store/global/selectors';
+import { ModelProviderCard } from '@/types/llm';
+
+const SYSTEM_AGENT_SETTING_KEY = 'systemAgent';
+
+type SettingItemGroup = ItemGroup;
+
+const useStyles = createStyles(({ css, prefixCls }) => ({
+  select: css`
+    .${prefixCls}-select-dropdown .${prefixCls}-select-item-option-grouped {
+      padding-inline-start: 12px;
+    }
+  `,
+}));
+interface ModelOption {
+  label: any;
+  provider: string;
+  value: string;
+}
+
+const SystemAgent = memo(() => {
+  const { t } = useTranslation('setting');
+  const [form] = AntForm.useForm();
+
+  const settings = useGlobalStore(settingsSelectors.currentSettings, isEqual);
+  const [setTranslationSystemAgent] = useGlobalStore((s) => [s.setTranslationSystemAgent]);
+
+  const select = useGlobalStore(modelProviderSelectors.modelSelectList, isEqual);
+
+  const { styles } = useStyles();
+
+  const enabledList = select.filter((s) => s.enabled);
+
+  const translationOptions = useMemo<SelectProps['options']>(() => {
+    const getChatModels = (provider: ModelProviderCard) =>
+      provider.chatModels
+        .filter((c) => !c.hidden)
+        .map((model) => ({
+          label: <ModelItemRender {...model} />,
+          provider: provider.id,
+          value: model.id,
+        }));
+
+    if (enabledList.length === 1) {
+      const provider = enabledList[0];
+
+      return getChatModels(provider);
+    }
+
+    return enabledList.map((provider) => ({
+      label: <ProviderItemRender provider={provider.id} />,
+      options: getChatModels(provider),
+    }));
+  }, [enabledList]);
+
+  const systemAgentSettings: SettingItemGroup = {
+    children: [
+      {
+        children: (
+          <Select
+            className={styles.select}
+            onChange={(model, option) =>
+              setTranslationSystemAgent((option as unknown as ModelOption).provider, model)
+            }
+            options={translationOptions}
+            popupMatchSelectWidth={false}
+          />
+        ),
+        desc: t('systemAgent.translation.modelDesc'),
+        label: t('systemAgent.translation.label'),
+        name: [SYSTEM_AGENT_SETTING_KEY, 'translation', 'model'],
+      },
+    ],
+    icon: MessageSquareMore,
+    title: t('systemAgent.title'),
+  };
+
+  useSyncSettings(form);
+
+  return (
+    <Form form={form} initialValues={settings} items={[systemAgentSettings]} {...FORM_STYLE} />
+  );
+});
+
+export default SystemAgent;

src/store/chat/slices/enchance/action.ts
@@ -9,7 +9,11 @@ import { chatService } from '@/services/chat';
 import { messageService } from '@/services/message';
 import { chatSelectors } from '@/store/chat/selectors';
 import { ChatStore } from '@/store/chat/store';
+import { useGlobalStore } from '@/store/global';
+import { settingsSelectors } from '@/store/global/selectors';
 import { ChatTTS, ChatTranslate } from '@/types/message';
+import { GlobalTranslationConfig } from '@/types/settings';
+import { merge } from '@/utils/merge';
 import { setNamespace } from '@/utils/storeDebug';
 
 const n = setNamespace('enhance');
@@ -21,6 +25,7 @@ export interface ChatEnhanceAction {
   clearTTS: (id: string) => Promise<void>;
   clearTranslate: (id: string) => Promise<void>;
   getCurrentTracePayload: (data: Partial<TracePayload>) => TracePayload;
+  getCurrentTranslationSetting: () => GlobalTranslationConfig;
   translateMessage: (id: string, targetLang: string) => Promise<void>;
   ttsMessage: (
     id: string,
@@ -48,11 +53,18 @@ export const chatEnhance: StateCreator<
     topicId: get().activeTopicId,
     ...data,
   }),
+  getCurrentTranslationSetting: () => {
+    return settingsSelectors.currentSystemAgent(useGlobalStore.getState()).translation;
+  },
+
   translateMessage: async (id, targetLang) => {
     const { toggleChatLoading, updateMessageTranslate, dispatchMessage } = get();
 
     const message = chatSelectors.getMessageById(id)(get());
     if (!message) return;
+
+    // Get current agent for translation
+    const translationSetting = get().getCurrentTranslationSetting();
 
     // create translate extra
     await updateMessageTranslate(id, { content: '', from: '', to: targetLang });
@@ -65,7 +77,7 @@ export const chatEnhance: StateCreator<
     // detect from language
     chatService
       .fetchPresetTaskResult({
-        params: chainLangDetect(message.content),
+        params: merge(translationSetting, chainLangDetect(message.content)),
         trace: get().getCurrentTracePayload({ traceName: TraceNameMap.LanguageDetect }),
       })
       .then(async (data) => {
@@ -87,7 +99,7 @@ export const chatEnhance: StateCreator<
           }),
         });
       },
-      params: chainTranslate(message.content, targetLang),
+      params: merge(translationSetting, chainTranslate(message.content, targetLang)),
       trace: get().getCurrentTracePayload({ traceName: TraceNameMap.Translator }),
     });
 

leexhq/LobeChat_0.159.12

src/app/(main)/settings/common/features/SystemAgent.tsx
@@ -1,0 +1,98 @@
+import { Form, type ItemGroup } from '@lobehub/ui';
+import { Form as AntForm, Select, SelectProps } from 'antd';
+import { createStyles } from 'antd-style';
+import isEqual from 'fast-deep-equal';
+import { MessageSquareMore } from 'lucide-react';
+import { memo, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { ModelItemRender, ProviderItemRender } from '@/components/ModelSelect';
+import { FORM_STYLE } from '@/const/layoutTokens';
+import { useUserStore } from '@/store/user';
+import { modelProviderSelectors, settingsSelectors } from '@/store/user/selectors';
+import { ModelProviderCard } from '@/types/llm';
+
+import { useSyncSettings } from '../../hooks/useSyncSettings';
+
+const SYSTEM_AGENT_SETTING_KEY = 'systemAgent';
+
+type SettingItemGroup = ItemGroup;
+
+const useStyles = createStyles(({ css, prefixCls }) => ({
+  select: css`
+    .${prefixCls}-select-dropdown .${prefixCls}-select-item-option-grouped {
+      padding-inline-start: 12px;
+    }
+  `,
+}));
+interface ModelOption {
+  label: any;
+  provider: string;
+  value: string;
+}
+
+const SystemAgent = memo(() => {
+  const { t } = useTranslation('setting');
+  const [form] = AntForm.useForm();
+
+  const settings = useUserStore(settingsSelectors.currentSettings, isEqual);
+  const [setTranslationSystemAgent] = useUserStore((s) => [s.setTranslationSystemAgent]);
+
+  const select = useUserStore(modelProviderSelectors.modelProviderListForModelSelect, isEqual);
+
+  const { styles } = useStyles();
+
+  const enabledList = select.filter((s) => s.enabled);
+
+  const translationOptions = useMemo<SelectProps['options']>(() => {
+    const getChatModels = (provider: ModelProviderCard) =>
+      provider.chatModels
+        .filter((c) => c.enabled)
+        .map((model) => ({
+          label: <ModelItemRender {...model} />,
+          provider: provider.id,
+          value: model.id,
+        }));
+
+    if (enabledList.length === 1) {
+      const provider = enabledList[0];
+
+      return getChatModels(provider);
+    }
+
+    return enabledList.map((provider) => ({
+      label: <ProviderItemRender provider={provider.id} />,
+      options: getChatModels(provider),
+    }));
+  }, [enabledList]);
+
+  const systemAgentSettings: SettingItemGroup = {
+    children: [
+      {
+        children: (
+          <Select
+            className={styles.select}
+            onChange={(model, option) =>
+              setTranslationSystemAgent((option as unknown as ModelOption).provider, model)
+            }
+            options={translationOptions}
+            popupMatchSelectWidth={false}
+          />
+        ),
+        desc: t('systemAgent.translation.modelDesc'),
+        label: t('systemAgent.translation.label'),
+        name: [SYSTEM_AGENT_SETTING_KEY, 'translation', 'model'],
+      },
+    ],
+    icon: MessageSquareMore,
+    title: t('systemAgent.title'),
+  };
+
+  useSyncSettings(form);
+
+  return (
+    <Form form={form} initialValues={settings} items={[systemAgentSettings]} {...FORM_STYLE} />
+  );
+});
+
+export default SystemAgent;

src/store/chat/slices/enchance/action.ts
@@ -9,7 +9,11 @@ import { chatService } from '@/services/chat';
 import { messageService } from '@/services/message';
 import { chatSelectors } from '@/store/chat/selectors';
 import { ChatStore } from '@/store/chat/store';
+import { useUserStore } from '@/store/user';
+import { settingsSelectors } from '@/store/user/selectors';
 import { ChatTTS, ChatTranslate } from '@/types/message';
+import { GlobalTranslationConfig } from '@/types/settings';
+import { merge } from '@/utils/merge';
 import { setNamespace } from '@/utils/storeDebug';
 
 const n = setNamespace('enhance');
@@ -21,6 +25,7 @@ export interface ChatEnhanceAction {
   clearTTS: (id: string) => Promise<void>;
   clearTranslate: (id: string) => Promise<void>;
   getCurrentTracePayload: (data: Partial<TracePayload>) => TracePayload;
+  getCurrentTranslationSetting: () => GlobalTranslationConfig;
   translateMessage: (id: string, targetLang: string) => Promise<void>;
   ttsMessage: (
     id: string,
@@ -48,11 +53,18 @@ export const chatEnhance: StateCreator<
     topicId: get().activeTopicId,
     ...data,
   }),
+  getCurrentTranslationSetting: () => {
+    return settingsSelectors.currentSystemAgent(useUserStore.getState()).translation;
+  },
+
   translateMessage: async (id, targetLang) => {
     const { toggleChatLoading, updateMessageTranslate, dispatchMessage } = get();
 
     const message = chatSelectors.getMessageById(id)(get());
     if (!message) return;
+
+    // Get current agent for translation
+    const translationSetting = get().getCurrentTranslationSetting();
 
     // create translate extra
     await updateMessageTranslate(id, { content: '', from: '', to: targetLang });
@@ -65,7 +77,7 @@ export const chatEnhance: StateCreator<
     // detect from language
     chatService
       .fetchPresetTaskResult({
-        params: chainLangDetect(message.content),
+        params: merge(translationSetting, chainLangDetect(message.content)),
         trace: get().getCurrentTracePayload({ traceName: TraceNameMap.LanguageDetect }),
       })
       .then(async (data) => {
@@ -87,7 +99,7 @@ export const chatEnhance: StateCreator<
           }),
         });
       },
-      params: chainTranslate(message.content, targetLang),
+      params: merge(translationSetting, chainTranslate(message.content, targetLang)),
       trace: get().getCurrentTracePayload({ traceName: TraceNameMap.Translator }),
     });
 